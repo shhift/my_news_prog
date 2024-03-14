33c42c9c0896f588e8e5bbe1e5070b102030d164
shhift <110626894+shhift@users.noreply.github.com>
2024-03-14 21:18:54 +0300

    v 0.0.5
    add Article

--- mywebsite/news/views.py
+++ mywebsite/news/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .admin import Article
+
+
+def article_list(request):
+    articles = Article.publish.all()
+    return render(request, "news/article/list.hml", {"articles": articles)
+
+
+def article_detail(request, id):
+    article = get_object_or_404(Article, id=id)
+    return render(request, "news/article/list.hml", {"articles": article)
