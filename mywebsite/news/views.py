0baaa39817c83a3ee1d20a941aaffc4dc90ba022
shhift <110626894+shhift@users.noreply.github.com>
2024-03-28 21:20:09 +0300

    add data in views

--- mywebsite/news/views.py
+++ mywebsite/news/views.py
@@ -2,9 +2,16 @@ from django.shortcuts import render, get_object_or_404
 from .models import Article
 
 
-def article_detail(request, id):
-    article = get_object_or_404(Article, id=id, status=Article.Status.PUBLISHED)
-    return render(request, "news/article/list.html", {"article": article})
+def article_detail(request, year, month, day, article_slg):
+    article = get_object_or_404(
+        Article,
+        status=Article.Status.PUBLISHED,
+        slug=article_slg,
+        publish_year=year,
+        publish_month=month,
+        publish_day=day,
+    )
+    return render(request, "news/article/detail.html", {"article": article})
 
 
 def article_list(request):
