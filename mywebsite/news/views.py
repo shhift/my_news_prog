5ba2ab3a9a6f0dceb6b13ab5fa41ff50968bd73e
shhift <110626894+shhift@users.noreply.github.com>
2024-03-28 20:20:57 +0300

    v0.0.20

--- mywebsite/news/views.py
+++ mywebsite/news/views.py
@@ -3,10 +3,10 @@ from .models import Article
 
 
 def article_detail(request, id):
-    article = get_object_or_404(Article, id=id)
+    article = get_object_or_404(Article, id=id, status=Article.Status.PUBLISHED)
     return render(request, "news/article/list.html", {"article": article})
 
 
 def article_list(request):
-    articles = Article.objects.all()
+    articles = Article.objects.published()
     return render(request, "news/article/list.html", {"articles": articles})
