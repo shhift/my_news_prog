6a78e966c03f12e0aaaf64d83593fb68eb48086f
shhift <110626894+shhift@users.noreply.github.com>
2024-04-18 20:26:02 +0300

    PL9
    add paginator

--- mywebsite/news/views.py
+++ mywebsite/news/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Article
+from django.core.paginator import Paginator
 
 
 def article_detail(request, year, month, day, article_slg):
@@ -16,4 +17,9 @@ def article_detail(request, year, month, day, article_slg):
 
 def article_list(request):
     articles = Article.published.all()
+
+    paginator = Paginator(articles, 2)
+    page_number = request.GET.get("page", 1)
+    articles = paginator.page(page_number)
+    
     return render(request, "news/article/list.html", {"articles": articles})
